757032a981b97b865ef888fa7362baabc3f1ad5f
sferratore.francescopaolo00@gmail.com <sferratore.francescopaolo00@gmail.com>
2024-12-07 16:44:12 +0100

    Additional Red phase of US#2

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -32,5 +32,19 @@ class TestCleaningRobot(TestCase):
         self.assertEqual(r.recharge_led_on, True)
         self.assertEqual(r.cleaning_system_on, False)
 
+    @patch.object(GPIO, "output")
+    @patch.object(IBS, "get_charge_left")
+    def test_manage_clean_system_when_charge_less_than_10_percent(self, mock_ibs: Mock, mock_output: Mock):
+        mock_ibs.side_effect = [11]  # Per qualche motivo mi veniva restituito un oggetto MagicMock con return_value. Ho quindi usato side_effect
+        r = CleaningRobot()
+        r.initialize_robot()
+        r.manage_cleaning_system()
+        mock_output.assert_has_calls([
+            unittest.mock.call(12, False),
+            unittest.mock.call(13, True)
+        ])
+        self.assertEqual(r.recharge_led_on, False)
+        self.assertEqual(r.cleaning_system_on, True)
+
 
 
