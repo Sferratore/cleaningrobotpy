55d33d80976749dd501d787efe21be8f46ca2734
sferratore.francescopaolo00@gmail.com <sferratore.francescopaolo00@gmail.com>
2024-12-07 16:21:57 +0100

    Red phase of US#2

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -1,3 +1,4 @@
+import unittest.mock
 from unittest import TestCase
 from unittest.mock import Mock, patch, call
 
@@ -15,6 +16,20 @@ class TestCleaningRobot(TestCase):
         r.initialize_robot()
         #assert
         self.assertEqual("0,0,N", r.robot_status())
+
+    @patch.object(IBS, "get_charge_left")
+    @patch.object(GPIO, "output")
+    def test_manage_clean_system_when_charge_less_than_10_percent(self, mock_ibs: Mock, mock_output: Mock):
+        mock_ibs.return_value = 9
+        r = CleaningRobot()
+        r.initialize_robot()
+        r.manage_cleaning_system()
+        mock_output.assert_has_calls([
+            unittest.mock.call(12, True),
+            unittest.mock.call(13, False)
+        ])
+        self.assertEqual(r.recharge_led_on, True)
+        self.assertEqual(r.cleaning_system_on, False)
 
 
 
